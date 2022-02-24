61d9be3492efc357a0efaab67e9a09b82f24d94f
GeXing <gexing@purvar.com>
2022-02-24 22:52:07 +0800

    upt: update build config

--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,26 @@
 const path = require('path')
 const resolve = dir => path.resolve(__dirname, dir)
-
-module.exports =
-  process.env.NODE_ENV === 'development'
-    ? {
-        pages: {
-          index: {
-            entry: './src/examples/main.js',
-            template: 'public/index.html',
-            filename: 'index.html'
-          }
-        },
-        devServer: {
-          port: 8080,
-          hot: true,
-          open: true,
-          disableHostCheck: true
+const isDev = process.env.NODE_ENV === 'development'
+module.exports = isDev
+  ? {
+      pages: {
+        index: {
+          entry: './src/examples/main.js',
+          template: 'public/index.html',
+          filename: 'index.html'
+        }
+      },
+      devServer: { port: 8080, hot: true, open: true, disableHostCheck: true }
+    }
+  : {
+      productionSourceMap: false,
+      css: { extract: false },
+      configureWebpack: {
+        output: {
+          filename: 'pro-layout/index.js',
+          libraryTarget: 'commonjs2',
+          path: path.join(__dirname, 'dist'),
+          publicPath: '/'
         }
       }
-    : {
-        productionSourceMap: false,
-        chainWebpack: config => {
-          // config.plugin('inline-source').use(require('html-webpack-inline-source-plugin'))
-        },
-        configureWebpack: {
-          output: {
-            filename: 'pro-layout/index.js',
-            libraryTarget: 'commonjs2',
-            path: path.join(__dirname, 'dist'),
-            publicPath: '/'
-          }
-        },
-        css: {
-          extract: false
-        }
-      }
+    }
